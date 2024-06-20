307b8d17e336999d11e9171690324c6418976ade
Marissa Crevecoeur <mac2538@columbia.edu>
2024-06-20 06:08:03 +0000

    Added reporting for user Codio group IDs (im so sad)

--- userInfo.sh
+++ userInfo.sh
@@ -3,4 +3,5 @@ echo "Now Generating User Report"; echo
 last
 echo "Reporting Current Running Processes"; echo
 ps auxw | grep "codio"
-last 
+echo "Reporting Group IDs for user codio"; echo
+id codio
